2d3046e8ceb2f0b6832d8437043567718119b2a3
Tappobill <christian.salvati@hotmail.it>
2022-10-24 18:22:21 +0200

    Array e Form

---
+++ jscript/main.js
@@ -0,0 +1,14 @@
+let listaEmail = ["chri@email.it", "christian@email.it", "salvati@email.it"];
+console.log(listaEmail);
+console.log(`la lista è formata da ${listaEmail.length} email `);
+console.log(listaEmail[0]);
+console.log(listaEmail[1]);
+console.log(listaEmail[2]);
+
+function recuperoEmail() {
+    let inputEmail = document.getElementById("inputEmail");
+    let email = inputEmail.value;
+
+    listaEmail.push(email);
+    console.log(listaEmail)
+}
